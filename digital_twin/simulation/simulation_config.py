7acecb0c49a60e179958736af638fb2d6380db82
Rishitha Rasineni <>
2026-06-27 16:10:44 +0530

    done with week 2

---
+++ digital_twin/simulation/simulation_config.py
@@ -0,0 +1,37 @@
+SIMULATION_YEARS = 5
+
+WEIGHT_OPTIONS = [
+    -5,
+    -10,
+    -15
+]
+
+BP_REDUCTION = [
+    5,
+    10,
+    15
+]
+
+CHOLESTEROL_REDUCTION = [
+    10,
+    20,
+    30
+]
+
+SCENARIOS = [
+
+    "No Intervention",
+
+    "Smoking Cessation",
+
+    "Weight Reduction",
+
+    "BP Control",
+
+    "Exercise Improvement",
+
+    "Combined Lifestyle",
+
+    "Combined Medical"
+
+]
